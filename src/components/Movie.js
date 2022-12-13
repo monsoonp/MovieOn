3d437624fe36029f4e3b05a0c422a58c680c475d
monsoon <alstn0706@naver.com>
2022-12-14 00:57:23 +0900

    main page ui change

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,19 +4,19 @@ import { Badge, Card, CardBody, CardFooter, CardTitle, Col } from "reactstrap";
 import { motion } from "framer-motion";
 import styles from "./Movie.module.css";
 
-function Movie({ idx, id, title, coverImg, year, summary, genres }) {
+function Movie({ idx, id, title, coverImg, year, summary, genres, imdb }) {
   const slicedSummary = summary.split(" ").length > 70 ? `${summary.split(" ").slice(0, 70).join(" ")} ...` : summary;
 
   const card = (idx) => {
     return {
-      hidden: { opacity: 0, scale: 0, x: -500, rotate: -30 },
+      hidden: { opacity: 0, scale: 0, x: 0, rotate: -30 },
       show: {
         opacity: 1,
         scale: 1,
         x: 0,
         rotate: 0,
         transition: {
-          delay: idx * 0.2,
+          // delay: idx * 0.2,
           // delayChildren: 0.3,
           // staggerChildren: 0.2,
           // ease: "easeInOut",
@@ -30,14 +30,17 @@ function Movie({ idx, id, title, coverImg, year, summary, genres }) {
 
   return (
     <Col className="my-5 mx-auto" sm="5">
-      <motion.div variants={card(idx)} initial="hidden" animate="show">
+      <motion.div variants={card(idx)} initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.8 }}>
         <Card>
           <CardTitle className="mx-3 mb-0">
             <div className={styles.movie}>
               <h2 className={styles.movie__title}>
                 <Link to={`/movie/${id}`}>{title}</Link>
               </h2>
-              <img className={styles.movie__img} src={coverImg} alt={title} />
+
+              <a href={`https://www.imdb.com/title/${imdb}`} target="_blank" rel="noopener noreferrer">
+                <img className={styles.movie__img} src={coverImg} alt={title} />
+              </a>
             </div>
           </CardTitle>
           {/* <CardSubtitle></CardSubtitle> */}
@@ -63,7 +66,7 @@ function Movie({ idx, id, title, coverImg, year, summary, genres }) {
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  coverImg: PropTypes.string.isRequired,
+  coverImg: PropTypes.string,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
