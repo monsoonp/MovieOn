33bfb946879812a4e6b0ed38602668948c0d3a98
monsoon <alstn0706@naver.com>
2023-03-21 02:14:11 +0900

    remove view transition

--- src/routes/DomesticMovie.js
+++ src/routes/DomesticMovie.js
@@ -42,12 +42,6 @@ function Domestic() {
   const [movies, setMovies] = useState([]);
   axios.defaults.withCredentials = true;
 
-  /*
-  const navigate = useNavigate();
-  document.startViewTransition(() => {
-    return navigate("/boxoffice");
-  });
-*/
   // TODO: 영화 상세 내용 추가
   const getMovies = async () => {
     // https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=20221101
